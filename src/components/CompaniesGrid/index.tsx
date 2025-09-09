bff005bf5476bb1dd36b94dec9459d905f121918
Eric Chang <55172722+echang594@users.noreply.github.com>
2025-09-09 16:35:03 -0700

    Update sponsors for 2025

--- src/components/CompaniesGrid/index.tsx
+++ src/components/CompaniesGrid/index.tsx
@@ -1,5 +1,5 @@
 import s from "./style.module.scss";
-import companies from "./companies_2024.json"
+import companies from "./companies_2025.json";
 
 const CompaniesGrid: React.FC = () => {
   return (
