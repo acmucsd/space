1c4aafddca4060e619f42caeed532d6604ec80d5
aeson tran <aesonvtran@gmail.com>
2026-06-25 19:54:16 -0700

    update for 2026
    updates logistical info; adds new company JSON file for finance later. no google form yet?

--- src/components/CompaniesGrid/index.tsx
+++ src/components/CompaniesGrid/index.tsx
@@ -1,5 +1,5 @@
 import s from "./style.module.scss";
-import companies from "./companies_2025.json";
+import companies from "./companies_2026.json";
 
 const CompaniesGrid: React.FC = () => {
   return (
