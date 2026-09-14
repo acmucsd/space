cd95897f6529d56f0e6ad6d3640322d02f7b19b8
Nathan Liu <nathanliu17@gmail.com>
2026-09-14 10:44:14 -0700

    add: new logos for 2026

---
+++ src/components/SupportingSponsorsGrid/index.tsx
@@ -0,0 +1,17 @@
+import s from "./style.module.scss";
+import companies from "./companies_2026.json";
+
+const SupportingSponsorsGrid: React.FC = () => {
+  return (
+    <div className={s.container}>
+      {companies.map((company, index) => (
+        <a className={s.companyItem} key={index} href={company.link}>
+          <img src={company.logo} className={s.companyLogo} alt={company.alt} />
+          <h5 className={s.companyName}>{company.name}</h5>
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default SupportingSponsorsGrid;
--- src/sections/Companies/index.tsx
+++ src/sections/Companies/index.tsx
@@ -1,11 +1,15 @@
 import CompaniesGrid from 'src/components/CompaniesGrid';
+import SupportingSponsorsGrid from 'src/components/SupportingSponsorsGrid';
 import s from './style.module.scss';
+import { Support } from '@mui/icons-material';
 
 const Companies: React.FC = () => {
   return (
     <section className={s.container} id="companies">
       <h1>Participating Companies</h1>
       <CompaniesGrid />
+      <h1>Supporting Sponsors</h1>
+      <SupportingSponsorsGrid />
     </section>
   );
 };
